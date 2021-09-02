0e5327297f881e25460621bdade7cd5d40c793ec
Matheus <matheusperdomo01@gmail.com>
2021-09-02 07:55:44 -0400

    master

--- App.js
+++ App.js
@@ -1,28 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-          <Text>Ola </Text>
-      </View>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+        <StatusBar style="auto" />
+        <View style={styles.header}>
+            <Image source={
+                require("./src/assets/arrow-left.png")
+                }>
+            </Image>
+            <View style={styles.inputTextView}>
+                <Image source={
+                    require("./src/assets/ei_search.png")
+                    }>
+                </Image>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Search"
+                    placeholderTextColor="#F5FFFF"
+                    >
+                    
+
+                </TextInput>
+            </View>
+            <Image source={
+                require("./src/assets/share.png")
+                }>
+            </Image>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header:{
-    height: 93,
-    backgroundColor: "#2C4877",
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+    },
+    header:{
+        minHeight:70,
+        backgroundColor:"#4369B0",
+        paddingVertical:30,
+        flexDirection:"row",
+        justifyContent:"space-between",
+        alignItems:"center",
+        paddingHorizontal:30,
+        paddingTop:50,
+    },
+    inputTextView:{
+        flex:1,
+        borderRadius:28,
+        backgroundColor:"#2C4877",
+        marginHorizontal: 10,
+        height:40,
+        alignItems:"center",
+        flexDirection:"row",
+    },
+    textInput:{
+        flex:1,
+        paddingRight:10,
+        color:"#F5FFFF",
+    },
 });

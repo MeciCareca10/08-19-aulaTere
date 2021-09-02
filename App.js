2122d1cf5ebd6a78e0f71a39122236517af747c0
Matheus <matheusperdomo01@gmail.com>
2021-09-02 10:37:18 -0400

    Link item

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
+const linkitem = ()=>{
+    return(
+        <Text> BUBLE </Text>
+    );
+};
+
 const linksimportantes = ()=>{
     return(
          <View>

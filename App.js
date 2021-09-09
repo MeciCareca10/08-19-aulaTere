eea2a4c5019c9cbeb8c8e06006077859c3598ed3
Matheus <matheusperdomo01@gmail.com>
2021-09-09 11:06:00 -0400

    Criando a rota e ela esta funcionando e tudo certo

--- App.js
+++ App.js
@@ -2,13 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {Text, View } from 'react-native';
 
-import Home from './src/Pages/Home';
-
-
+import routes from './src/routes'
 
 export default function App() {
     return (
-       <View>
+       <View style={{flex:1}}>
            <Home/>
         </View> 
     );

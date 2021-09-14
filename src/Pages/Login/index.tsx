6881a523e2825409e2d9f19eb122c041ad526962
Matheus <matheusperdomo01@gmail.com>
2021-09-14 19:48:34 -0400

    tentando tarefa

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet, TextInput } from 'react-native';
 
 const Login = () => {
     return(
-        <View>
-            <Image source={
-                require("")
+        <View style={styles.container}>
+            <View style={styles.facebook}>
+            <Image style={styles.img} source={
+                require("../../assets/faceb.png")
             }>
             </Image>
-            <Text>Facebook</Text>
+            <Text style={styles.face}>Facebook</Text>
+            </View>
+            
         </View>
     );
 }
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: "#4369B0",
+    },
+    facebook:{
+
+    },
+    img:{
+
+    },
+    face:{
+
+    },
+});
 
 export default Login;

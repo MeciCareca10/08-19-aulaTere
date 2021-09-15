8bd3b8906e95fe0ebfb4b4706c3443c0ffc83513
Matheus <matheusperdomo01@gmail.com>
2021-09-15 09:23:37 -0400

    Tarefa Commit da Tela de Login

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {Text, View, StyleSheet } from 'react-native';
+import {Text, View, StyleSheet, Routes } from 'react-native';
 
-import  Routes from './src/Routes'
-
+import  Routes from '../08-19-aulaTere/src/Routes'
 
 export default function App() {
     return (
